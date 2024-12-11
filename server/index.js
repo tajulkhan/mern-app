85f480aee4d6824bd4f9e57d2b652e9b09832230
tajulkhan <tajulbasheer@gmail.com>
2024-12-11 13:28:38 +0530

    ++1

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("MongoDB connection error:", err));
 
